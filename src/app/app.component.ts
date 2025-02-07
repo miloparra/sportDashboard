744431b9e7ffd1d9f44b0e7ee6d509c0604b3a44
miloparra <miloparra1531.mp@gmail.com>
2025-02-07 09:25:53 +0100

    App - Implementing profil component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { ProfilComponent } from "./profil/profil.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, RouterLink, RouterLinkActive],
+  imports: [RouterOutlet, RouterLink, RouterLinkActive, ProfilComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
