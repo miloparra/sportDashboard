9665e038b0b0d3ce7677d53d8c1f4785b3edfc99
miloparra <miloparra1531.mp@gmail.com>
2025-02-07 11:58:38 +0100

    Profil - Adding the run totals per year

--- src/app/profil/profil.component.ts
+++ src/app/profil/profil.component.ts
@@ -1,12 +1,31 @@
 import { Component } from '@angular/core';
+import { RunService, RunYear } from '../run/run.service';
+import { BikeService, RideYear } from '../bike/bike.service';
+import { SwimService, SwimYear } from '../swim/swim.service';
+import { HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-profil',
   standalone: true,
-  imports: [],
+  providers: [RunService, BikeService, SwimService],
+  imports: [HttpClientModule],
   templateUrl: './profil.component.html',
   styleUrl: './profil.component.scss'
 })
 export class ProfilComponent {
 
+  constructor(private runService: RunService, private rideService: BikeService, private swimService: SwimService) { }
+
+  runs: RunYear[] = [];
+  rides: RideYear[] = [];
+  swims: SwimYear[] = [];
+
+  selectedYear = 2;
+
+  ngOnInit(): void {
+    this.runService.getRunYearTotal().subscribe(runData => {
+      this.runs = runData;
+      console.log(this.runs)
+    });
+  }
 }
