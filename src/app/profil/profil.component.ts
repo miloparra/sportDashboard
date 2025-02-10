a4e8ea8028c0559593131d07da26085d6e0870c5
miloparra <miloparra1531.mp@gmail.com>
2025-02-10 12:48:59 +0100

    Profil - Implementing the activities totals per year in the profil section

--- src/app/profil/profil.component.ts
+++ src/app/profil/profil.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { RunService, RunYear } from '../run/run.service';
 import { BikeService, RideYear } from '../bike/bike.service';
 import { SwimService, SwimYear } from '../swim/swim.service';
@@ -8,7 +9,7 @@ import { HttpClientModule } from '@angular/common/http';
   selector: 'app-profil',
   standalone: true,
   providers: [RunService, BikeService, SwimService],
-  imports: [HttpClientModule],
+  imports: [CommonModule, HttpClientModule],
   templateUrl: './profil.component.html',
   styleUrl: './profil.component.scss'
 })
@@ -20,12 +21,49 @@ export class ProfilComponent {
   rides: RideYear[] = [];
   swims: SwimYear[] = [];
 
-  selectedYear = 2;
+  selectedYear = '2025';
+
+  selectedRunYear = {
+    distSum: '',
+    timeSum: '',
+    denivSum: ''
+  }
+
+  selectedRideYear = {
+    distSum: '',
+    timeSum: '',
+    denivSum: ''
+  }
+
+  selectedSwimYear = {
+    distSum: '',
+    timeSum: ''
+  }
 
   ngOnInit(): void {
     this.runService.getRunYearTotal().subscribe(runData => {
       this.runs = runData;
-      console.log(this.runs)
+      this.rideService.getRideYearTotal().subscribe(rideData => {
+        this.rides = rideData;
+        this.swimService.getSwimYearTotal().subscribe(swimData => {
+          this.swims = swimData;
+          this.updateSelectedYearActivities();
+        });
+      });
     });
   }
+
+  updateSelectedYearActivities() {
+    this.selectedRunYear.distSum = this.runs.find(item => item.year == this.selectedYear)?.distSum ?? '0';
+    this.selectedRunYear.timeSum = this.runs.find(item => item.year == this.selectedYear)?.timeSum ?? '00:00:00';
+    this.selectedRunYear.denivSum = this.runs.find(item => item.year == this.selectedYear)?.denivSum ?? '0';
+
+    this.selectedRideYear.distSum = this.rides.find(item => item.year == this.selectedYear)?.distSum ?? '0';
+    this.selectedRideYear.timeSum = this.rides.find(item => item.year == this.selectedYear)?.timeSum ?? '00:00:00';
+    this.selectedRideYear.denivSum = this.rides.find(item => item.year == this.selectedYear)?.denivSum ?? '0';
+
+    this.selectedSwimYear.distSum = this.swims.find(item => item.year == this.selectedYear)?.distSum ?? '0';
+    this.selectedSwimYear.timeSum = this.swims.find(item => item.year == this.selectedYear)?.timeSum ?? '00:00:00';
+  }
+
 }
