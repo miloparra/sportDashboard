e80f494c57be889dbadf6e15dd03e8760a831035
miloparra <miloparra1531.mp@gmail.com>
2025-02-12 19:41:15 +0100

    Navbar - Adding login signin routes and logout button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,27 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 import { ProfilComponent } from "./profil/profil.component";
+import { AuthService } from './services/auth.service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, RouterLink, RouterLinkActive, ProfilComponent],
+  providers: [AuthService],
+  imports: [CommonModule, RouterOutlet, RouterLink, RouterLinkActive, ProfilComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'sportdashboard';
 
+  constructor(public authService: AuthService) { }
+
   openApp() {
     window.open('http://localhost:8080', '_blank', 'width=600,height=550');
+  }
+
+  logout() {
+    this.authService.logout();
   }
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,13 +5,18 @@ import { BikeComponent } from './bike/bike.component';
 import { SwimComponent } from './swim/swim.component';
 import { FitnessComponent } from './fitness/fitness.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { LoginComponent } from './login/login.component';
+import { SigninComponent } from './signin/signin.component';
+import { AuthGuard } from './guard/auth.guard';
 
 export const routes: Routes = [
-    {path: '', redirectTo: '/general', pathMatch: 'full'}, //A l'ouverture de l'app, l'utilisateur est dirige directement sur la page General
-    {path: 'general', component: GeneralComponent},
-    {path: 'running', component: RunComponent},
-    {path: 'bike', component: BikeComponent},
-    {path: 'swim', component: SwimComponent},
-    {path: 'fitness', component: FitnessComponent},
-    {path: 'dashboard', component: DashboardComponent},
+    { path: '', redirectTo: '/general', pathMatch: 'full' }, //A l'ouverture de l'app, l'utilisateur est dirige directement sur la page General
+    { path: 'general', component: GeneralComponent },
+    { path: 'running', component: RunComponent, canActivate: [AuthGuard] },
+    { path: 'bike', component: BikeComponent, canActivate: [AuthGuard] },
+    { path: 'swim', component: SwimComponent, canActivate: [AuthGuard] },
+    { path: 'fitness', component: FitnessComponent, canActivate: [AuthGuard] },
+    { path: 'dashboard', component: DashboardComponent, canActivate: [AuthGuard] },
+    { path: 'login', component: LoginComponent },
+    { path: 'signin', component: SigninComponent },
 ];
