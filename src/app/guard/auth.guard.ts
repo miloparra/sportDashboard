7d0daaeb554fdd3110896b31d6df28f63ac8d325
miloparra <miloparra1531.mp@gmail.com>
2025-03-02 20:51:41 +0100

    Auth - Remove useless AuthService import from the auth.guard.ts file

--- src/app/guard/auth.guard.ts
+++ src/app/guard/auth.guard.ts
@@ -1,10 +1,9 @@
 import { Injectable } from '@angular/core';
 import { CanActivate, Router } from '@angular/router';
-import { AuthService } from '../services/auth.service';
 
 @Injectable({ providedIn: 'root' })
 export class AuthGuard implements CanActivate {
-    constructor(private authService: AuthService, private router: Router) { }
+    constructor(private router: Router) { }
 
     canActivate(): boolean {
         if (localStorage.getItem('token')) return true;
